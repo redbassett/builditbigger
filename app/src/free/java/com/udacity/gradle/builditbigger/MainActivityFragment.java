4599094cda76e36f221340264d28d3530fb61e53
Harry Grillo <harry@redbassett.com>
2016-08-25 13:38:34 -0400

    Add interstitial ad layer before joke is shown

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    InterstitialAd mInstersitialAd;
+    Button mButton;
 
     public MainActivityFragment() {
     }
@@ -23,6 +28,18 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
 
+        mButton = (Button) root.findViewById(R.id.this_button_tells_jokes);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mInstersitialAd.isLoaded()) {
+                    mInstersitialAd.show();
+                }
+                ((MainActivity) getActivity()).tellJoke(v);
+            }
+        });
+
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
@@ -31,6 +48,28 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        mInstersitialAd = new InterstitialAd(getActivity());
+        mInstersitialAd.setAdUnitId(getString(R.string.intersitial_ad_unit_id));
+        mInstersitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewIntersitialAd();
+            }
+        });
+
+        requestNewIntersitialAd();
+
         return root;
     }
+
+    private void requestNewIntersitialAd() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInstersitialAd.loadAd(adRequest);
+    }
+
+
 }
